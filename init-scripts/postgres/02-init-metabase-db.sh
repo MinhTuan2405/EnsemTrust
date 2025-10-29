0bd885d8a52f11ce2ca998905aca09ea078de711
mnhtndev <tuannguyen.02042005@gmail.com>
2025-10-30 01:21:14 +0700

    fix: data pipeline services infrastructure

--- init-scripts/postgres/02-init-metabase-db.sh
+++ init-scripts/postgres/02-init-metabase-db.sh
@@ -2,6 +2,6 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    SELECT 'CREATE DATABASE metabase_db'
+    SELECT 'CREATE DATABASE metabase'
     WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'metabase')\gexec
 EOSQL
